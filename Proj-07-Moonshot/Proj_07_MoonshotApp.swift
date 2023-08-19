c47b6427b7f4d5a5c9206fd213b6db457d821d0b
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-08-19 16:17:26 +0300

    Initial Commit

---
+++ Proj-07-Moonshot/Proj_07_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proj_07_MoonshotApp.swift
+//  Proj-07-Moonshot
+//
+//  Created by Андрей Бородкин on 19.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Proj_07_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
